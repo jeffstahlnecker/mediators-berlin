e532425ec7436ef23977e2de1b0a66268ea2ae0d
stahldom <jeff@stahlnecker.me>
2020-09-06 21:56:04 +0200

    fixed profile page no render

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -2,5 +2,5 @@ import React from "react";
 import Layout from "../components/Layout";
 
 export default function Home() {
-  return <Layout>This will be the profile page</Layout>;
+  return <Layout>This will be the template for individuals profiles</Layout>;
 }
